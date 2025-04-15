5bbd891c3ace716f42992e60ecccafa66623847b
rafinas2133 <90997180+rafinas2133@users.noreply.github.com>
2025-04-15 20:08:46 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import { resolve } from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: resolve(__dirname, 'src'),
+  base: '/story-app/',
   publicDir: resolve(__dirname, 'src', 'public'),
   build: {
-    outDir: resolve(__dirname, 'dist'),
+    outDir: resolve(__dirname, 'docs'),
     emptyOutDir: true,
   },
   resolve: {
